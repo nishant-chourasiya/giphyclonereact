664c7fecc471c56d3c171e073436a2aba9f5a865
nishant-chourasiya <nishantchourasiya5@gmail.com>
2026-02-14 00:48:24 +0530

    fix: responsive updates + header improvements

--- src/components/gif.jsx
+++ src/components/gif.jsx
@@ -1,32 +1,3 @@
-// import React from 'react'
-// import { Link } from 'react-router-dom';
-
-// const Gif = ({gif,hover = true}) => {
-//   return (
-//     <Link to={`${gif.type}/${gif.slug}`}>
-//         <div className='w-full mb-2 relative cursor-pointer'>
-//             <img src={gif?.images?.fixed_width.webp}
-//           alt={gif?.title}
-//           className="w-full object-cover rounded transition-all duration-300" />
-
-
-//             {hover && <div className="absolute inset-0 rounded opacity-0 hover:opacity-100 transition-opacity duration-300 bg-gradient-to-b from-transparent via-transparent to-black to-emerald-200 font-bold flex items-end gap-2 p-2">
-         
-//                 <img src={gif?.user?.avatar_url} 
-//                 alt={gif?.user?.display_name} 
-//                  className='h-8'/>
-
-//                 <span>{gif?.user?.display_name} </span>
-                 
-//                 </div>}
-//         </div>
-//     </Link>
-//   )
-// }
-
-// export default Gif;
-
-
 import React from 'react'
 import { Link } from 'react-router-dom';
 
